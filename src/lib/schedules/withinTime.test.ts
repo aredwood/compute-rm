f8275818370fd1a64a28b2409fb18ad746e95409
Alex Redwood <hello@alexredwood.com>
2019-09-29 21:14:21 +1000

    test

--- src/lib/schedules/withinTime.test.ts
+++ src/lib/schedules/withinTime.test.ts
@@ -3,6 +3,6 @@ import withinTime from "./withinTime"
 
 describe("positive",() => {
     test("is within zone",() => {
-        expect(withinTime(new Date("2019-09-29T10:35:39.530+10"),"20:30","21:00")).toBe(true)
+        expect(withinTime(new Date("2019-09-29T10:35:39.530+10:00"),"20:30","21:00")).toBe(true)
     })
 })
